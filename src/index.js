abe1cc1f921d2847cd7a4d96edd95699de2145c8
shivasayz <writeforshivaa@gmail.com>
2025-11-19 22:33:16 +0530

    middleware cors and api erros added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
+const PORT = process.env.PORT;
 dotenv.config({
  path: "./env",
 });
 
-connectDB();
+connectDB()
+ .then(() => {
+  app.listen(PORT, () => {
+   console.log(`server is running at https://localhost:${PORT}`);
+  });
+
+  app.on((err) => {
+   console.log(`error: `, err);
+   throw err;
+  });
+ })
+ .catch((err) => {
+  console.log("MONGO db connection failed !", err);
+ });
